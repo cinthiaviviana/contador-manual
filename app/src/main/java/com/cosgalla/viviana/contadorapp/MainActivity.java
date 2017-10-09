d74bc71285f8fda8977ea7d6f2662b6d38d579b5
Cinthia cosgalla <13470206@itcampeche.edu.mx>
2017-10-09 12:32:40 -0500

    ContadorApp : Se agrego interfaz de usuario grafica. No incluye manejo de eventos

---
+++ app/src/main/java/com/cosgalla/viviana/contadorapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.cosgalla.viviana.contadorapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    Button mBotoncontar;
+    Button mBotonreiniciar;
+    EditText mcampoConteo;
+    int CONTADOR = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        CONTADOR++;
+        mcampoConteo = (EditText) findViewById(R.id.etiqueta_boton);
+
+        mBotoncontar = (Button) findViewById(R.id.boton_contar);
+        mBotoncontar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                CONTADOR++;
+                mcampoConteo.setText(+CONTADOR);
+            }
+        });
+        mBotonreiniciar = (Button) findViewById(R.id.boton_reiniciar);
+
+        mBotonreiniciar = (Button) findViewById(R.id.boton_reiniciar);
+        mBotoncontar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                CONTADOR = 0;
+                mcampoConteo.setText(+CONTADOR);
+                                            }
+                                        }
+        );
+    }
+}
